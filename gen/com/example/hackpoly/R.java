f8614704798806aea54a97362fbee954cf00db67
Ashley Barton <barton.ash@gmail.com>
2014-01-25 10:39:11 -0800

    finished UI for compose

--- gen/com/example/hackpoly/R.java
+++ gen/com/example/hackpoly/R.java
@@ -21,21 +21,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int newform=0x7f020001;
-        public static final int roundbutton=0x7f020002;
-        public static final int search=0x7f020003;
-        public static final int search_btn=0x7f020004;
+        public static final int downvote=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int newform=0x7f020002;
+        public static final int roundbutton=0x7f020003;
+        public static final int search=0x7f020004;
+        public static final int search_btn=0x7f020005;
+        public static final int upvote=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f080007;
         public static final int address=0x7f080001;
-        public static final int button1=0x7f080004;
-        public static final int button2=0x7f080005;
+        public static final int button1=0x7f080003;
+        public static final int button2=0x7f080004;
         public static final int editText1=0x7f080000;
-        public static final int editText2=0x7f080003;
-        public static final int imageButton1=0x7f080006;
+        public static final int editText2=0x7f080005;
         public static final int textfield=0x7f080002;
+        public static final int upvote_icon=0x7f080006;
     }
     public static final class layout {
         public static final int activity_compose=0x7f030000;
@@ -51,6 +53,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int address_name=0x7f050007;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int title_activity_compose=0x7f050004;
