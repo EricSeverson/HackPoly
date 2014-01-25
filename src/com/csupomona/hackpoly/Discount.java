68181a4f3cb9ccb752b4458331ae7a4db961220f
Aaron Gaut <aarongaut@b01-aruba2-guest-10-110-16-155.wlan.csupomona.edu>
2014-01-25 14:43:26 -0800

    Fixed discount object

--- src/com/csupomona/hackpoly/Discount.java
+++ src/com/csupomona/hackpoly/Discount.java
@@ -1,5 +1,28 @@
 package com.csupomona.hackpoly;
 
 public class Discount {
+	private int upvotes;
+	private int downvotes;
+	private String establishment;
+	private String[] address;
+	private String description;
+	
+	public Discount(int up, int down, String est, String[] address, String desc){
+		upvotes = up;
+		downvotes = down;
+		establishment = est;
+		for (int i = 0; i < address.length; i++){
+			this.address[i] = address[i];
+		}
+		description = desc;
+	}
+	
+	public int getUpvotes() { return upvotes; }
+	public int getDownvotes() { return downvotes; }
+	public String getEstablishment() { return establishment; }
+	public String getDescription() { return description; }
+	public int getAddressLineCount() { return address.length; }
+	public String getAddressLine(int l) { return address[l]; }
+	
 
 }
--- src/com/csupomona/hackpoly/DiscountActivity.java
+++ src/com/csupomona/hackpoly/DiscountActivity.java
@@ -8,10 +8,27 @@ import android.view.Menu;
 
 public class DiscountActivity extends Activity {
 
+	private Discount[] sampleDiscounts = new Discount[5];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_discount);
+		
+		String[] Address0 = {"21550 Valley Boulevard", "City of Industry, CA 91789"};
+		sampleDiscounts[0] = new Discount(2, 1, "Chic fil' A", Address0, "Free Medium Drink");
+		
+		String[] Address1 = {"21555 Valley Boulevard", "Walnut, CA"};
+		sampleDiscounts[1] = new Discount(1, 0, "Staples", Address1, "20% off on school supplies");
+		
+		String[] Address2 = {"3560 W Temple Ave", "Pomona, CA 91768"};
+		sampleDiscounts[2] = new Discount(1, 1, "Curry Up", Address2, "10% off");
+		
+		String[] Address3 = {"1600 S Azusa Ave", "City of Industry, CA"};
+		sampleDiscounts[3] = new Discount(0, 0, "Toys R Us", Address3, "5% off");
+		
+		String[] Address4 = {"13401 Crossroads Pkwy N", "City of Industry, CA"};
+		sampleDiscounts[4] = new Discount(0, 0, "Fry's Electronics", Address4, "Free antivirus with purchase of computer");
 	}
 
 	@Override
