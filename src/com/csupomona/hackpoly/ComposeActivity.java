052c1a6638b062eb04ff354e4267a7c5039f7bd1
EricSeverson <severson_eric@yahoo.com>
2014-01-24 23:20:56 -0800

    Added Location capabilities to ComposeActivity

--- src/com/csupomona/hackpoly/ComposeActivity.java
+++ src/com/csupomona/hackpoly/ComposeActivity.java
@@ -2,16 +2,29 @@ package com.csupomona.hackpoly;
 
 import com.example.hackpoly.R;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class ComposeActivity extends Activity {
+	
+	private LocationManager locationManager;
+	private String provider;
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compose);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		provider = locationManager.getBestProvider(criteria, false);
+		Location location = locationManager.getLastKnownLocation(provider);
 	}
 
 	@Override
