88db0a28dd6083f9d673296ae9d8d81f56db6873
EricSeverson <severson_eric@yahoo.com>
2014-01-24 20:34:52 -0800

    Added the necessary activities

---
+++ src/com/example/hackpoly/DiscountActivity.java
@@ -0,0 +1,22 @@
+package com.example.hackpoly;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DiscountActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_discount);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.discount, menu);
+		return true;
+	}
+
+}
