414cd13e7ff6bb8af23603e1f65d6f8220e1f624
Aaron Sun <aaronsun@b01-aruba2-guest-10-110-30-138.wlan.csupomona.edu>
2014-01-25 02:56:25 -0800

    Added compose page

--- gen/com/example/hackpoly/R.java
+++ gen/com/example/hackpoly/R.java
@@ -23,11 +23,18 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int roundbutton=0x7f020001;
+        public static final int search=0x7f020002;
+        public static final int search_btn=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080002;
+        public static final int button2=0x7f080005;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080003;
+        public static final int editText3=0x7f080001;
+        public static final int imageButton1=0x7f080006;
+        public static final int textfield=0x7f080004;
     }
     public static final class layout {
         public static final int activity_compose=0x7f030000;
